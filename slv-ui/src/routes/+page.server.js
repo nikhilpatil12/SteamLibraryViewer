0eb0ae9242a41a9290ede71a60efd7550d566520
nikhilpatil12 <patil.nikhil12@outlook.com>
2023-11-01 16:20:02 -0700

    fixes

--- slv-ui/src/routes/+page.server.js
+++ slv-ui/src/routes/+page.server.js
@@ -2,7 +2,8 @@
 import { API_KEY } from '$env/static/private';
 export async function load({ fetch }) {
 	const res = await fetch('https://steamapi.nikpatil.com/profile', {
-		method: 'GET'
+		method: 'GET',
+		credentials: 'include'
 	});
 
 	const data = await res.json();
