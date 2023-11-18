149cf310551cc5043f4208b077a5a22c88d1171a
nikhilpatil12 <patil.nikhil12@outlook.com>
2023-11-17 16:33:50 -0800

    added library in profile

--- slv-ui/src/routes/profile/+page.svelte
+++ slv-ui/src/routes/profile/+page.svelte
@@ -16,6 +16,7 @@
 	import { toast } from '@zerodevx/svelte-toast';
 	import { redirect } from '@sveltejs/kit';
 	import { goto } from '$app/navigation';
+	import { ShareAllSolid, LinkOutline } from 'flowbite-svelte-icons';
 	import { browser, dev, building, version } from '$app/environment';
 
 	let isUserLoggedIn = false;
@@ -51,46 +52,31 @@
 	 */
 	let userData;
 	onMount(() => {
-		// Extract user data from the query parameter
 		fetch(!dev ? 'https://steamapi.nikpatil.com/profile' : 'http://localhost:3000/profile', {
 			method: 'GET',
 			credentials: 'include'
 		})
 			.then((response) => response.json())
 			.then((data) => {
-				// console.log(JSON.parse(data.user));
 				isUserLoggedIn = true;
 				if (data.user) {
 					if (data.code == 200) {
 						userData = JSON.parse(data.user.profile);
-						// goto(`/gameList/${userData.id}`);
 					} else {
 						console.error('User data not found in the query parameter');
 					}
 				} else {
-					// User is not authenticated, redirect to login or show a message
 					goto('/');
 				}
 			});
-		// const queryParams = new URLSearchParams(location.search);
-		// let userData = queryParams.get('user');
-		// console.log(userData);
 	});
 </script>
 
 <body>
-	{#if userData}
+	{#if isUserLoggedIn}
 		<div>
 			<p class="text-lg dark:text-white">Welcome, {userData.displayName}</p>
 			<Card padding="sm">
-				<div class="flex justify-end">
-					<!-- <DotsHorizontalOutline /> -->
-					<Dropdown class="w-36">
-						<DropdownItem>Edit</DropdownItem>
-						<DropdownItem>Export data</DropdownItem>
-						<DropdownItem>Delete</DropdownItem>
-					</Dropdown>
-				</div>
 				<div class="flex flex-col items-center pb-4">
 					<Avatar size="lg" src={userData.photos[2].value} />
 					<h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">
@@ -107,7 +93,12 @@
 						<Button on:click={copyProfileUrl} color="light" class="dark:text-white">
 							Share Profile
 						</Button>
-						<Button on:click={logoutUser} color="light" class="dark:text-white">Logout</Button>
+						<Button on:click={logoutUser} color="red" class="dark:text-white">Logout</Button>
+					</div>
+					<div class="flex space-x-3 w-max mt-4 lg:mt-6">
+						<Button color="blue" href="/gameList/{userData.id}" class="dark:text-white">
+							View Library
+						</Button>
 					</div>
 				</div>
 			</Card>
